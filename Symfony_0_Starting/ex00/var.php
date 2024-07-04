27130b821e683ec7cc49b6c02eccb367b0a99d72
rmeriau <remy.meriau@hotmail.com>
2024-07-04 11:59:38 -0400

    ex00

---
+++ Symfony_0_Starting/ex00/var.php
@@ -0,0 +1,12 @@
+<?php
+echo "My first variables:\n";
+$a = 'Bob';
+$b = 'Joe';
+$c = 1;
+$d = 1.1;
+echo "a contains : $a and has type " . gettype($a) . "\n";
+echo "b contains : $b and has type " . gettype($b) . "\n";
+echo "c contains : $c and has type " . gettype($c) . "\n";
+echo "d contains : $d and has type " . gettype($d) . "\n";
+
+?>
