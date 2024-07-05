f9bc3f38604659340515214af55cbcea1f341e8a
Remy Meriau <remy.meriau@hotmail.fr>
2024-07-05 14:04:13 +0200

    ex04

---
+++ Symfony_0_Starting/ex04/capital_city_from.php
@@ -0,0 +1,27 @@
+<?php
+function capital_city_from($capital) {
+    $states = [
+    'Oregon' => 'OR',
+    'Alabama' => 'AL',
+    'New Jersey' => 'NJ',
+    'Colorado' => 'CO',
+    ];
+    $capitals = [
+    'OR' => 'Salem',
+    'AL' => 'Montgomery',
+    'NJ' => 'trenton',
+    'KS' => 'Topeka',
+    ];
+
+    if (array_key_exists($capital, $states)) {
+        $new_key = $states[$capital];
+        if (array_key_exists($new_key, $capitals)) {
+            return $capitals[$new_key] . "\n";
+        } else {
+            return "Unknown\n";
+        }
+    } else {
+        return "Unknown\n";
+    }
+}
+?>
